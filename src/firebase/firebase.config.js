bbf6656c7b9fe2a0e7291260bfb4976b64770b71
saifulislam735 <mohimshihab735@gmail.com>
2024-06-15 01:28:43 +0600

    env added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAPX_tR1Th36dH5NiSodpNdF4W4gGDVyrA",
-    authDomain: "dragonnews-auth-80087.firebaseapp.com",
-    projectId: "dragonnews-auth-80087",
-    storageBucket: "dragonnews-auth-80087.appspot.com",
-    messagingSenderId: "145931625188",
-    appId: "1:145931625188:web:f50d61ba7e25a9b50f02f1"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
